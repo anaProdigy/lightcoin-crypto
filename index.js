7fd91966e44d926d8d735125161693b355091981
Anastasia <z.anastasia11@gmail.com>
2023-03-13 22:56:53 -0700

    add driver code

--- index.js
+++ index.js
@@ -38,21 +38,33 @@ class Withdrawal extends Transaction {
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
-const myAccount = new Account("snow-patrol");
+// const myAccount = new Account("snow-patrol");
 
 
-t1 = new Withdrawal(50.25, myAccount);
+// t1 = new Withdrawal(50.25, myAccount);
+// t1.commit();
+// console.log('Transaction 1:', t1);
+
+// t2 = new Withdrawal(9.99, myAccount);
+// t2.commit();
+// console.log('Transaction 2:', t2);
+
+// console.log('Balance:', myAccount.balance);
+
+// t3 = new Deposit(120.00, myAccount);
+// t3.commit();
+// console.log('Transaction 3:', t3);
+
+// console.log('Balance:', myAccount.balance);
+
+const myAccount = new Account('billybob');
+
+console.log('Starting Balance:', myAccount.balance);
+
+const t1 = new Deposit(120.00, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99, myAccount);
+const t2 = new Withdrawal(50.00, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-console.log('Balance:', myAccount.balance);
-
-t3 = new Deposit(120.00, myAccount);
-t3.commit();
-console.log('Transaction 3:', t3);
-
-console.log('Balance:', myAccount.balance);
+console.log('Ending Balance:', myAccount.balance);
